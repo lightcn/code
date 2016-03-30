8301a1888aefb816b5e1cf00c57bcae1f158dde5
张 志亮 <zhilight@gmail.com>
2016-03-30 17:31:14 +0800

    更新代码

---
+++ Python/convert_class_func.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+
+from urllib.request import urlopen
+
+
+class UrlTemplate:
+    def __init__(self, template):
+        self.template = template
+
+    def open(self, **kwargs):
+        return urlopen(self.template.format_map(kwargs))
+
+
+yahoo = UrlTemplate('http://finance.yahoo.com/d/quotes.csv?s={names}&f={fields}')
+for line in yahoo.open(names='IBM,AAPL,FB', fields='sl1c1v'):
+    print(line.decode('utf8'))
